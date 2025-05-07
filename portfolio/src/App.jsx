b23dd157342dd8f0cd7af18dabd024730b15f9bb
suman khatiwada Github <you@example.com>
2025-05-07 14:40:24 +0545

    skills  part done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,31 @@
 import Nav from './components/nav.jsx';
 import { useState } from 'react';
 import Homepage from './components/homepage.jsx';
+import Skills from './components/skills.jsx';
+import Html from './media/html.svg';
+import Css from './media/css.svg';
+import Reactlogo from './media/react.svg';
+import Javascript from './media/javascript.svg';
 
 export const App = () => {
   const [showmenu, setShowMenu] = useState(false);
 
+  const skillsData = [
+    { img: Html, name: 'HTML' },
+    { img: Css, name: 'CSS' },
+    { img: Javascript, name: 'JavaScript' },
+    { img: Reactlogo, name: 'React' },
+    
+  ];
+
   return (
     <>
-    <Homepage />
-    <Nav showmenu={showmenu} setshowmenu={setShowMenu} /> 
+      <Homepage />
+      <Nav showmenu={showmenu} setshowmenu={setShowMenu} /> 
+      <Skills skills={skillsData} />
     </>
   );
 };
 
 export default App;
+
